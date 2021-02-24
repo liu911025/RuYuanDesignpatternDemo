dfd0eb53fdf23f2aa1465795b080f52d6259b4a5
ljt <329311024@qq.com>
2021-02-24 17:33:00 +0800

    修改

--- src/main/java/com/designpattern/study/iterator/headFast/one/WaitressTest.java
+++ src/main/java/com/designpattern/study/iterator/headFast/one/WaitressTest.java
@@ -3,7 +3,7 @@ package com.designpattern.study.iterator.headFast.one;
 public class WaitressTest {
 
     public static void main(String[] args) {
-        Waitress waitress = new Waitress();
+        Waitress waitress = new Waitress(new DinerMenu(), new PancakeHouseMenu());
         waitress.printMenu();
     }
 
