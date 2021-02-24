ab77172d7e4436d96ac04e98987f446bd235332a
ljt <329311024@qq.com>
2021-02-24 14:26:47 +0800

    Waitress第一版

--- src/main/java/com/designpattern/study/iterator/headFast/one/Waitress.java
+++ src/main/java/com/designpattern/study/iterator/headFast/one/Waitress.java
@@ -1,12 +1,33 @@
 package com.designpattern.study.iterator.headFast.one;
 
+import java.util.ArrayList;
+
 public class Waitress {
+
+    public static void main(String[] args) {
+        Waitress waitress = new Waitress();
+        waitress.printMenu();
+    }
 
     /**
      * 打印出菜单上的每一项
      */
     public void printMenu() {
+        DinerMenu dinerMenu = new DinerMenu();
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
 
+        // 遍历DinerMenu
+        MenuItem[] dinerMenuItems = dinerMenu.getMenuItems();
+        for (MenuItem menuItem : dinerMenuItems) {
+            System.out.println(menuItem.getName() + " -- " + menuItem.getPrice() + " -- " + menuItem.getDescription());
+        }
+
+        // 遍历PancakeHouseMenu
+        ArrayList pancakeHouseMenuItems = pancakeHouseMenu.getMenuItems();
+        for (Object Object : pancakeHouseMenuItems) {
+            MenuItem menuItem = (MenuItem) Object;
+            System.out.println(menuItem.getName() + " -- " + menuItem.getPrice() + " -- " + menuItem.getDescription());
+        }
     }
 
     /**
