055755cfeb96cb6d1381aa9a12fd1434078dd495
liujiatai <329311024@qq.com>
2021-02-24 21:29:07 +0800

    Waitress第三版,Java自身迭代器

---
+++ src/main/java/com/designpattern/study/iterator/headFast/three/Menu.java
@@ -0,0 +1,8 @@
+package com.designpattern.study.iterator.headFast.three;
+
+import java.util.Iterator;
+
+public interface Menu {
+
+    Iterator createIterator();
+}
---
+++ src/main/java/com/designpattern/study/iterator/headFast/three/PancakeHouseMenuIterator.java
@@ -0,0 +1,37 @@
+package com.designpattern.study.iterator.headFast.three;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * PancakeHouseMenu迭代器
+ */
+public class PancakeHouseMenuIterator implements Iterator {
+
+    private ArrayList items;
+    private int position = 0; // 遍历数组位置
+
+    public PancakeHouseMenuIterator(ArrayList items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (position >= items.size() || items.get(position) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Object next() {
+        Object o = items.get(position);
+        position++;
+        return o;
+    }
+
+    @Override
+    public void remove() {
+        items.remove(position);
+    }
+}
